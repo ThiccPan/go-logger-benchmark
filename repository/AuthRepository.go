8d95fc300381122ada0dab09f27ae898b34af450
ThiccPan <ihilmiderian@gmail.com>
2024-03-31 16:36:54 +0700

    feat: auth repo

---
+++ repository/AuthRepository.go
@@ -0,0 +1,41 @@
+package repository
+
+import (
+	"github.com/thiccpan/go-logger-benchmark/domain"
+	"github.com/thiccpan/go-logger-benchmark/logger"
+	"gorm.io/gorm"
+)
+
+type SQLiteAuthRepo struct {
+	logger logger.Ilogger
+	db     *gorm.DB
+}
+
+func NewSQLiteAuthRepo(logger logger.Ilogger, db *gorm.DB) *SQLiteAuthRepo {
+	repo := SQLiteAuthRepo{
+		db:     db,
+		logger: logger,
+	}
+	return &repo
+}
+
+func (sau *SQLiteAuthRepo) AddUser(user *domain.User) error {
+	db := sau.db.Create(user)
+	if err := db.Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (sau *SQLiteAuthRepo) GetUserByEmail(email string) (domain.User, error) {
+	var user domain.User
+	db := sau.db.First(&user, "email = ?", email)
+	if err := db.Error; err != nil {
+		return user, err
+	}
+	return user, nil
+}
+
+func (sau *SQLiteAuthRepo) UpdateUser(email string, newUser *domain.User) error {
+	return nil
+}
