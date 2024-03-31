d6cd553e381ac1a74604d75f8e9a98c96f5e825f
ThiccPan <ihilmiderian@gmail.com>
2024-03-31 16:36:40 +0700

    fix: update get user to return error

--- repository/IAuthRepository.go
+++ repository/IAuthRepository.go
@@ -4,6 +4,6 @@ import "github.com/thiccpan/go-logger-benchmark/domain"
 
 type IAuthRepository interface {
 	AddUser(user *domain.User) error
-	GetUserByEmail(email string) domain.User
+	GetUserByEmail(email string) (domain.User, error)
 	UpdateUser(email string, newUser *domain.User) error
 }
