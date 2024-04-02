0b2a22c0474a890151e2a11e0f1c9022c6107a14
ThiccPan <ihilmiderian@gmail.com>
2024-04-02 12:47:54 +0700

    feat: add fields to logging interface

--- logger/ILogger.go
+++ logger/ILogger.go
@@ -1,9 +1,9 @@
 package logger
 
 type Ilogger interface {
-	LogInfo(msg string)
-	LogErr(msg string)
-	LogDebug(msg string)
-	LogWarn(msg string)
-	LogFatal(msg string)
+	LogInfo(msg string, fields ...map[string]any)
+	LogErr(msg string, fields ...map[string]any)
+	LogDebug(msg string, fields ...map[string]any)
+	LogWarn(msg string, fields ...map[string]any)
+	LogFatal(msg string, fields ...map[string]any)
 }
