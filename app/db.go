bb56cb050e8041f81805286fdce265a267bb19cd
ThiccPan <ihilmiderian@gmail.com>
2024-05-06 13:36:54 +0700

    feat: disable db logging

--- app/db.go
+++ app/db.go
@@ -4,12 +4,14 @@ import (
 	"github.com/thiccpan/go-logger-benchmark/domain"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 func InitDB() *gorm.DB {
-	db, err := gorm.Open(sqlite.Open("log-experiment.db"), &gorm.Config{})
+	db, err := gorm.Open(sqlite.Open("log-experiment.db"), &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Silent),
+	})
 	if err != nil {
-
 		panic("failed to connect database")
 	}
 	// Migrate the schema
