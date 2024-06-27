4f05370e9d4557712f9533e599a4f9775e46474c
ThiccPan <ihilmiderian@gmail.com>
2024-06-28 01:44:52 +0700

    fix: wrong logging location

--- service/ItemService.go
+++ service/ItemService.go
@@ -19,8 +19,6 @@ type itemService struct {
 	logger *logrus.Logger
 }
 
-// AddItem implements IItemService.
-
 func NewItemService(repo repository.IItemRepo, logger *logrus.Logger) *itemService {
 	return &itemService{
 		repo:   repo,
@@ -30,60 +28,50 @@ func NewItemService(repo repository.IItemRepo, logger *logrus.Logger) *itemServi
 
 func (is *itemService) AddItem(newItem domain.Item) (domain.Item, error) {
 	item, err := is.repo.AddItem(newItem)
-	// is.logger.LogDebug("adding item on repository successfull")
-	is.logger.Debug("adding item on repository successfull")
 	if err != nil {
-		// is.logger.LogDebug("error on adding item on repository")
 		is.logger.Debug("error on adding item on repository")
 		return item, err
 	}
+	is.logger.Debug("adding item on repository successfull")
 	return item, nil
 }
 
 func (is *itemService) GetItems() ([]domain.Item, error) {
 	items, err := is.repo.GetItems()
-	// is.logger.LogDebug("fetching items on repository successfull")
-	is.logger.Debug("fetching items on repository successfull")
 	if err != nil {
-		// is.logger.LogDebug("error on fetching items on repository")
 		is.logger.Debug("error on fetching items on repository")
 		return nil, err
 	}
+	is.logger.Debug("fetching items on repository successfull")
 	return items, nil
 }
 
 func (is *itemService) GetItem(id uint) (domain.Item, error) {
 	item, err := is.repo.GetItem(uint(id))
-	// is.logger.LogDebug("fetching item on repository successfull")
-	is.logger.Debug("fetching item on repository successfull")
 	if err != nil {
-		// is.logger.LogDebug("error fetching item on repository")
 		is.logger.Debug("error fetching item on repository")
 		return item, err
 	}
+	is.logger.Debug("fetching item on repository successfull")
 	return item, nil
 }
 
 func (is *itemService) UpdateItem(id uint, updateToItem domain.Item) (domain.Item, error) {
 	item, err := is.repo.UpdateItem(uint(id), &updateToItem)
-	// is.logger.LogDebug("updating item on repository successfull")
-	is.logger.Debug("updating item on repository successfull")
 	if err != nil {
-		// is.logger.LogDebug("error updating item on repository")
 		is.logger.Debug("error updating item on repository")
 		return item, err
 	}
+	is.logger.Debug("updating item on repository successfull")
 	return item, nil
 }
 
 func (is *itemService) DeleteItem(id uint) (domain.Item, error) {
 	item, err := is.repo.DeleteItem(id)
-	// is.logger.LogDebug("deleting item on reporsitory successfull")
-	is.logger.Debug("deleting item on reporsitory successfull")
 	if err != nil {
-		// is.logger.LogDebug("error deleting item on repository")
 		is.logger.Debug("error deleting item on repository")
 		return item, err
 	}
+	is.logger.Debug("deleting item on reporsitory successfull")
 	return item, nil
 }
